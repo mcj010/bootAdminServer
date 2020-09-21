b7a08708958f06fed710b7f7b5e6efca2321a51b
mcj010 <329392494@qq.com>
2020-09-21 20:23:27 +0800

    create project.

---
+++ src/main/java/com/example/bootAdminServer/BootAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootAdminServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootAdminServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bootAdminServer/BootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootAdminServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
